630842d8e8cfbdc911aa50c5505f25e3efd7f851
Nanne <elaine.s.santin@gmail.com>
2023-04-26 22:19:58 -0300

    Refatoração das classes e inclusão da classe conteudo.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import edu.elaine.dio.desafio.dominio.Conteudo;
 import edu.elaine.dio.desafio.dominio.Curso;
 import edu.elaine.dio.desafio.dominio.Mentoria;
 
@@ -10,7 +11,20 @@ public class Main {
         curso1.setDescricao("Descrição curso java");
         curso1.setCargaHoraria(8);
 
-        Curso curso2 = new Curso("Curso POO", "Descrição curso POO", 8);
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Python");
+        curso2.setDescricao("Descrição curso Python");
+        curso2.setCargaHoraria(12);
+
+        Conteudo curso3 = new Curso(); //exemplo de polimorfismo
+        curso3.setTitulo("Curso POO");
+        curso3.setDescricao("Descrição curso POO");
+        /*
+        Por estar usando Conteudo para instanciar o curso não consigo inserir a carga
+        horária.
+        Tudo o que está na classe mãe (Conteudo) posso pegar na classe filha (Curso), mas
+        o que está na classe filha, não posso pegar na classe mãe.
+         */
 
         Mentoria mentoria1 = new Mentoria();
         mentoria1.setTitulo("Mentoria Java");
@@ -19,6 +33,7 @@ public class Main {
 
         System.out.println(curso1);
         System.out.println(curso2);
+        System.out.println(curso3);
         System.out.println(mentoria1);
 
     }
--- src/edu/elaine/dio/desafio/dominio/Curso.java
+++ src/edu/elaine/dio/desafio/dominio/Curso.java
@@ -1,33 +1,19 @@
 package edu.elaine.dio.desafio.dominio;
 
-public class Curso {
-    private String titulo;
-    private String descricao;
+public class Curso extends Conteudo {
+
     private int cargaHoraria;
 
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
+    }
+
     public Curso(String titulo, String descricao, int cargaHoraria) {
-        this.titulo = titulo;
-        this.descricao = descricao;
         this.cargaHoraria = cargaHoraria;
     }
 
     public Curso() {
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
     }
 
     public int getCargaHoraria() {
@@ -41,8 +27,8 @@ public class Curso {
     @Override
     public String toString() {
         return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", cargaHoraria=" + cargaHoraria +
                 '}';
     }
